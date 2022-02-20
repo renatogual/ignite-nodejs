1feac75f18efd9836efb196d1639db937ca8834e
renatogual <renatogual@hotmail.com>
2022-02-20 19:58:14 -0300

    feat: add routes

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,9 +1,41 @@
 const express = require('express')
+const { v4: uuidV4 } = require('uuid')
 
 const app = express()
 
+app.use(express.json())
+
 app.listen(3333)
 
-app.get('/', (req, res) => {
-  return res.send('hello world')
+const customers = []
+
+app.get('/statement/:cpf', (req, res) => {
+  const { cpf } = req.params
+
+  const customer = customers.find((customer) => customer.cpf === cpf)
+
+  if (!customer) {
+    return res.status(400).json({ error: 'Cliente não encontrado!' })
+  }
+
+  return res.json(customer.statement)
+})
+
+app.post('/account', (req, res) => {
+  const { name, cpf } = req.body
+
+  const cpfAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+  if (cpfAlreadyExists) {
+    return res.status(400).json({ error: 'CPF já existente!' })
+  }
+
+  customers.push({
+    id: uuidV4(),
+    name,
+    cpf,
+    statement: [],
+  })
+
+  return res.status(201).send('Conta criada com sucesso')
 })
