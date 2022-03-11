d03c7f076807a5709b04a2489fb2b085ffd578e8
renatogual <renatogual@hotmail.com>
2022-03-11 14:35:35 -0300

    feat: add upload useCase

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'
+import multer from 'multer'
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
+import { importCategoryController } from '../modules/cars/useCases/importCategory'
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 
 const categoriesRoutes = Router()
+const upload = multer({
+  dest: './tmp',
+})
 
 categoriesRoutes.post('/', (req, res) =>
   createCategoryController.handle(req, res)
@@ -12,5 +17,9 @@ categoriesRoutes.post('/', (req, res) =>
 categoriesRoutes.get('/', (req, res) => {
   listCategoriesController.handle(req, res)
 })
+
+categoriesRoutes.post('/import', upload.single('file'), (req, res) =>
+  importCategoryController.handle(req, res)
+)
 
 export { categoriesRoutes }
