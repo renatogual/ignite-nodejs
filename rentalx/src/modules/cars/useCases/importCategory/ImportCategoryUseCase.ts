f047c4801e2e43b41a331e311c60ce856deae080
renatogual <renatogual@hotmail.com>
2022-03-11 16:46:15 -0300

    feat: add upload file

--- rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,44 @@
+/* eslint-disable prettier/prettier */
+import { parse } from 'csv-parse'
+import fs from 'fs'
+
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
+
+interface IImportCategory {
+  name: string
+  description: string
+}
+
+
 export class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file)
+  constructor(private categoriesRepository: ICategoriesRepository) { }
+
+  loadCategories(file: Express.Multer.File) {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path)
+      const categories: IImportCategory[] = []
+
+      const parseFile = parse()
+
+      stream.pipe(parseFile)
+
+      parseFile.on('data', async (line) => {
+        const [name, description] = line
+        categories.push({
+          name,
+          description
+        })
+      }).on('end', () => {
+        resolve(categories)
+      })
+        .on('error', (err) => {
+          reject(err)
+        })
+    })
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+    console.log(categories)
   }
 }
